18457060142312fb607bb4a058752d564d67e58e
Dani Aziz <dany1002008@gmail.com>
2024-11-02 18:36:13 +0100

    increased chrome window size

--- main.py
+++ main.py
@@ -115,7 +115,7 @@ options.binary_location = '/opt/chrome/chrome'
 options.add_argument("--headless=new")
 options.add_argument('--no-sandbox')
 options.add_argument("--disable-gpu")
-options.add_argument("--window-size=1280x1696")
+options.add_argument("--window-size=1920x1080")  # Full HD resolution
 options.add_argument("--single-process")
 options.add_argument("--disable-dev-shm-usage")
 options.add_argument("--disable-dev-tools")
