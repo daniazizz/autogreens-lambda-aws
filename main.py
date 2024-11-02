bfc7730dec8ff4d14256bf22820314fa0cde8d11
Dani Aziz <dany1002008@gmail.com>
2024-11-02 14:48:57 +0100

    initial autogreens code

--- main.py
+++ main.py
@@ -1,27 +1,229 @@
 from selenium import webdriver
-from tempfile import mkdtemp
 from selenium.webdriver.common.by import By
+import gspread
+from google.oauth2.service_account import Credentials
+from time import sleep
+import random
+from selenium import webdriver
+from selenium.webdriver.firefox.service import Service as FirefoxService
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver import FirefoxOptions
+# from openpyxl import Workbook
+import datetime
+from tempfile import mkdtemp
 
 
-def handler(event=None, context=None):
-    options = webdriver.ChromeOptions()
-    service = webdriver.ChromeService("/opt/chromedriver")
+import json
+import boto3
+from google.oauth2 import service_account
+from googleapiclient.discovery import build
 
-    options.binary_location = '/opt/chrome/chrome'
-    options.add_argument("--headless=new")
-    options.add_argument('--no-sandbox')
-    options.add_argument("--disable-gpu")
-    options.add_argument("--window-size=1280x1696")
-    options.add_argument("--single-process")
-    options.add_argument("--disable-dev-shm-usage")
-    options.add_argument("--disable-dev-tools")
-    options.add_argument("--no-zygote")
-    options.add_argument(f"--user-data-dir={mkdtemp()}")
-    options.add_argument(f"--data-path={mkdtemp()}")
-    options.add_argument(f"--disk-cache-dir={mkdtemp()}")
-    options.add_argument("--remote-debugging-port=9222")
+# Fetch Google API credentials JSON from AWS Secrets Manager
+def get_google_credentials():
+    secret_name = "my-google-api-credentials"  # Name of your secret in Secrets Manager
+    region_name = "eu-west-3"  # AWS region of the Secrets Manager
 
-    chrome = webdriver.Chrome(options=options, service=service)
-    chrome.get("https://facebook.com/")
+    # Create a Secrets Manager client
+    client = boto3.client("secretsmanager", region_name=region_name)
+    
+    # Retrieve the secret
+    response = client.get_secret_value(SecretId=secret_name)
+    
+    # Parse the JSON credentials
+    secret_json = json.loads(response["SecretString"])
+    credentials = service_account.Credentials.from_service_account_info(secret_json)
+    return credentials
 
-    return chrome.find_element(by=By.XPATH, value="//html").text
+# Fetch Google API credentials JSON from AWS Secrets Manager
+def get_autogreens_config():
+    secret_name = "autogreens-config"  # Name of your secret in Secrets Manager
+    region_name = "eu-west-3"  # AWS region of the Secrets Manager
+
+    # Create a Secrets Manager client
+    client = boto3.client("secretsmanager", region_name=region_name)
+    
+    # Retrieve the secret
+    response = client.get_secret_value(SecretId=secret_name)
+    
+    # Parse the JSON credentials
+    secret_json = json.loads(response["SecretString"])
+    credentials = service_account.Credentials.from_service_account_info(secret_json)
+    return credentials
+
+
+creds = Credentials.from_service_account_file(get_google_credentials())
+
+
+# Define the scope
+scope = [
+   'https://spreadsheets.google.com/feeds',
+   'https://www.googleapis.com/auth/spreadsheets',
+   'https://www.googleapis.com/auth/drive.file',
+   'https://www.googleapis.com/auth/drive'
+]
+creds = creds.with_scopes(scope)
+
+
+# Authorize the client
+client = gspread.authorize(creds)
+
+
+
+
+
+# Higher order functions, taking sheet as an argument
+
+def create_row(sheet, data):
+   sheet.append_row(data)
+
+def read_data(sheet):
+   return sheet.get_all_records()
+
+def update_cell(sheet, row, col, new_value):
+    sheet.update_cell(row, col, new_value)
+
+def delete_row(sheet, row):
+    sheet.delete_rows(row)
+  
+
+GREENYARD_PRICE_COL = 4
+VP_COL = 7
+MARGE_COL = 8
+LAST_UPDATE_COL = 9
+
+
+import json
+
+
+
+config = json.load(get_autogreens_config())
+
+
+# Access the parameters
+BROWSER_DRIVER_PATH = config.get('browser_driver_path')
+GY_USERNAME_MARKET = config.get('gy_username_market')
+GY_PASSWORD_MARKET = config.get('gy_password_market')
+
+
+def human_sleep(min_time=1, max_time=3):
+   sleep_time = random.uniform(min_time, max_time)
+   sleep(sleep_time)
+
+
+
+
+options = webdriver.ChromeOptions()
+options.binary_location = '/opt/chrome/chrome'
+options.add_argument("--headless=new")
+options.add_argument('--no-sandbox')
+options.add_argument("--disable-gpu")
+options.add_argument("--window-size=1280x1696")
+options.add_argument("--single-process")
+options.add_argument("--disable-dev-shm-usage")
+options.add_argument("--disable-dev-tools")
+options.add_argument("--no-zygote")
+options.add_argument(f"--user-data-dir={mkdtemp()}")
+options.add_argument(f"--data-path={mkdtemp()}")
+options.add_argument(f"--disk-cache-dir={mkdtemp()}")
+options.add_argument("--remote-debugging-port=9222")
+
+
+def init_eos(username, password):
+
+
+   service = webdriver.ChromeService("/opt/chromedriver")
+   driver = webdriver.Chrome(options=options, service=service)
+
+
+
+
+   # Step 1: Log in to the website
+   driver.get("https://eos.firstinfresh.be/login")
+   human_sleep(2, 4)
+
+
+   # Enter username
+   username_input = driver.find_element(By.XPATH, "/html/body/div[2]/div/div/div/form/div[1]/div[1]/input")
+   ActionChains(driver).move_to_element(username_input).click().perform()
+   human_sleep(1, 2)
+   username_input.send_keys(username)  # Replace with your username
+   human_sleep(1, 3)
+
+
+   # Enter password
+   password_input = driver.find_element(By.XPATH, "/html/body/div[2]/div/div/div/form/div[1]/div[2]/input")
+   ActionChains(driver).move_to_element(password_input).click().perform()
+   human_sleep(1, 2)
+   password_input.send_keys(password)  # Replace with your password
+   human_sleep(1, 3)
+
+
+   # Submit the form
+   login_button = driver.find_element(By.XPATH, "/html/body/div[2]/div/div/div/form/div[2]/input")
+   ActionChains(driver).move_to_element(login_button).click().perform()
+   human_sleep(3, 5)
+  
+   return driver
+
+
+import re
+
+def extract_price(input_string):
+    # Regular expression to match the price pattern (e.g., "€ 1,79")
+    price_pattern = r"€\s*\d+[.,]\d{2}"
+    
+    # Search for the price in the input string
+    match = re.search(price_pattern, input_string)
+    
+    # Return the price if found, otherwise return None
+    return match.group(0) if match else None
+
+
+def run_eos(username, password, sheet):
+    driver = init_eos(username, password)
+    data = read_data(sheet)
+    i = 2
+    for e in data:
+        print(e)
+        # Step 2: Navigate to the desired page
+        driver.get(f"https://eos.firstinfresh.be/shop/item/{e.get('GY-REF')}")
+        human_sleep(2, 4)
+
+
+        # Step 3: Scrape the required information
+        #Try or skip
+        
+        try:
+         vp_data_element = driver.find_element(By.XPATH, "/html/body/div[2]/div[2]/div/div[2]/div/div[2]/div[6]/table/tbody/tr[4]/td[2]")
+         scraped_data = vp_data_element.text
+         scraped_data = extract_price(scraped_data)
+         print(scraped_data)
+         update_cell(sheet, i, VP_COL, scraped_data)
+         data_element = driver.find_element(By.XPATH, "/html/body/div[2]/div[2]/div/div[2]/div/div[2]/div[6]/table/tbody/tr[2]/td[2]")
+         scraped_data = data_element.text
+         print(scraped_data)
+         update_cell(sheet, i, GREENYARD_PRICE_COL, scraped_data)
+         ct = datetime.datetime.now()
+         update_cell(sheet, i, LAST_UPDATE_COL, str(ct))
+        except:
+         print("Error")
+         update_cell(sheet, i, GREENYARD_PRICE_COL, "Error")
+         
+        i+=1
+
+
+    driver.quit()  
+
+
+
+# SHEET WITH NAME "MARKET" AND "EXPRESS"
+sheet_market = client.open('PRIX-GREENYARD').get_worksheet(0)
+run_eos(GY_USERNAME_MARKET, GY_PASSWORD_MARKET, sheet_market)
+# sheet_express.sort((PRIJS_VERSHIL_COL, 'des'))
+
+
+
+
+
+
